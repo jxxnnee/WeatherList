cb605c2c6ca9095dd7dc15b8a967e8a417039677
minkyungjun <q_alz@naver.com>
2022-11-10 19:36:42 +0900

    프로젝트 생성

---
+++ WeatherList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherList
+//
+//  Created by 민경준 on 2022/11/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
